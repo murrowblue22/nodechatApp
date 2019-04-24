080092d3db6562adacf34b59210bbcd459dabd30
James Pinkney <murrowblue24@yahoo.com>
2019-04-24 02:55:49 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,7 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
     
-    socket.emit('createMessage', {
-        from: "nip@Nodechat.com",
-        text: "You think life is important, its how u lived thats important"
-    });
-});
+ });
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
