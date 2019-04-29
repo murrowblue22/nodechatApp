edfca4614877933e148556a62bc3d255352aff49
James Pinkney <murrowblue24@yahoo.com>
2019-04-29 17:18:10 +0000

    create generateMessae utility

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const express       = require("express"),
         server      = http.createServer(app),
         io          = socketIO(server);
 
+const {generateMessage} = require('./utils/message');
+
 const path = require('path');
 const publicPath = path.join(__dirname, '../public'); 
 
@@ -32,27 +34,15 @@ io.on('connection', (socket) => {
     console.log("New User connected"); 
     
     
-    socket.emit('newMessage', {
-        from: 'Admin', 
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
     
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined', 
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
     
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         
-        io.emit('newMessage', {
-            from: message.from, 
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text, 
---
+++ server/utils/message.test.js
@@ -0,0 +1,25 @@
+const expect = require('expect');
+
+const {generateMessage} = require('./message');
+
+describe('generateMessage' , () => {
+    
+    it('should generate correct message object', () => {
+        
+        const from = 'Jen';
+        const text = 'Good Times';
+        const message = generateMessage(from, text);
+        
+        
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({
+            from, 
+            text
+        });
+        
+    });
+    
+        
+});
+
+
